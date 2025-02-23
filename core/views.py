306af46e1a432565c5279ab142a1629094fa617c
Jason Guo <jason.ghzr@gmail.com>
2025-02-23 04:02:33 -0800

    Finish the Product-list page views, now I can show real products to the web page based on the Database

--- core/views.py
+++ core/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, wishlist, Address
+
 def index(request):
-    return render(request, 'core/index.html')
+    # products = Product.objects.all().order_by('-id')
+    products = Product.objects.filter(product_status="published", featured=True)
+
+    context = {
+        'products': products
+    }
+    
+    return render(request, 'core/index.html', context)
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published")
+
+    context = {
+        'products': products
+    }
+    
+    return render(request, 'core/product-list.html', context)
+
+def category_list_view(request):
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories 
+    }
+    
+    return render(request, 'core/category-list.html', context)
+
