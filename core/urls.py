7ed77a6f4b5b568dec49deff00af72993d24252e
Jason Guo <jason.ghzr@gmail.com>
2025-02-23 17:31:40 -0800

    create the product list category view, allow users to access items through Category section, also showing number of products based on data in db

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 # from core.views import index
-from core.views import index, category_list_view, product_list_view
+from core.views import index, category_list_view, product_list_view, category_product_list_view
 
 app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
     path("products/", product_list_view, name="product-list"),
-    path("category/", category_list_view, name="category-list")
-    
+    path("category/", category_list_view, name="category-list"),
+    path("category/<cid>/", category_product_list_view, name="category-product-list")
 ]
