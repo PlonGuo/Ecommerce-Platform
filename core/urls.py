ecc85d4364cd07dbee02d51c04ec4578f5aecf5a
Jason Guo <jason.ghzr@gmail.com>
2025-02-23 21:48:55 -0800

    add vendor lists, so now user can see vendors with contact method, name, and number of stocks

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 # from core.views import index
-from core.views import index, category_list_view, product_list_view, category_product_list_view
+from core.views import index, category_list_view, product_list_view, category_product_list_view, vendor_list_view
 
 app_name = "core"
 
 urlpatterns = [
+    # Homepage
     path("", index, name="index"),
     path("products/", product_list_view, name="product-list"),
+
+    # Category
     path("category/", category_list_view, name="category-list"),
-    path("category/<cid>/", category_product_list_view, name="category-product-list")
+    path("category/<cid>/", category_product_list_view, name="category-product-list"),
+
+    # Vendor
+    path("vendors/", vendor_list_view, name="vendor-list"),
 ]
--- core/views.py
+++ core/views.py
@@ -41,3 +41,11 @@ def category_product_list_view(request, cid):
         'category': category
     }
     return render(request, 'core/category-product-list.html', context)
+
+def vendor_list_view(request):
+    vendors = Vendor.objects.all()
+
+    context = {
+        'vendors': vendors
+    }
+    return render(request, 'core/vendor-list.html', context)
